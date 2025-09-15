ad81b0b19a379890b6dfa7803173ff51dd70f4d0
Mayank-171986 <guptamay@iu.edu>
2025-09-15 19:30:52 +0000

    Week-3-assignment

--- apputil.py
+++ apputil.py
@@ -3,3 +3,15 @@ import pandas as pd
 
 
 # update/add code below ...
+def fib_rec(n):
+    if n<=0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_rec(n-1) + fib_rec(n-2)
+
+def fib(n):
+    fib_series = [fib_rec(i) for i in  range(n+1)]
+    print(fib_series)
+    return(fib_series[n])
